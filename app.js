51d8312d8a6a39fba7102b2b54fb624185f09f42
dwyfrequency <jdwy215@gmail.com>
2019-04-05 11:34:14 -0400

    Finished Lesson 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my API');
+  res.send('Welcome to my Nodemon API');
 });
 
 app.listen(port, () => {
